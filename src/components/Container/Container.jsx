a99b7ec8c762f2874d4e9e35969e137246fed3ce
K1ngVlad <kingvlad3008@gmail.com>
2023-04-17 22:44:00 +0400

    container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,17 +1,9 @@
-import { ContainerStyle } from './ContainerStyle';
+import { ContainerStyle, BoxStyle } from './ContainerStyle';
 
 const Container = ({ children }) => {
   return (
-    <ContainerStyle
-      width="100vw"
-      height="100vh"
-      display="flex"
-      justify="center"
-      background="white"
-    >
-      <ContainerStyle width="100%" height="100%" maxWidth="740px">
-        {children}
-      </ContainerStyle>
+    <ContainerStyle>
+      <BoxStyle>{children}</BoxStyle>
     </ContainerStyle>
   );
 };
--- src/components/Container/ContainerStyle.js
+++ src/components/Container/ContainerStyle.js
@@ -1,19 +1,21 @@
 import styled from 'styled-components';
 
-const ContainerStyle = styled.div.attrs((props) => ({
-  children: props.children,
+const ContainerStyle = styled.div.attrs(({ children }) => ({
+  children,
 }))`
-  width: ${({ width }) => width || 'auto'};
-  height: ${({ height }) => height || 'auto'};
-  min-height: ${({ minHeight }) => minHeight || 'auto'};
-  min-width: ${({ minWidth }) => minWidth || 'auto'};
-  max-height: ${({ maxHeight }) => maxHeight || 'auto'};
-  max-width: ${({ maxWidth }) => maxWidth || 'auto'};
-  display: ${({ display }) => display || 'block'};
-  flex-direction: ${({ direction }) => direction || 'row'};
-  align-items: ${({ align }) => align || 'flex-start'};
-  justify-content: ${({ justify }) => justify || 'flex-start'};
-  background: ${({ background }) => background || 'none'};
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background: white;
 `;
 
-export { ContainerStyle };
+const BoxStyle = styled.div.attrs(({ children }) => ({
+  children,
+}))`
+  width: 100%;
+  height: 100%;
+  max-width: 740px;
+`;
+
+export { ContainerStyle, BoxStyle };
